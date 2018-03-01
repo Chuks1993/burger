e05f0f8e47c4a175c0fff5f1adfe0967ba07a07f
Chuks1993 <chuksgrinage@gmail.com>
2018-03-01 13:17:15 -0600

    hw

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beef');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Sandwich', true);
